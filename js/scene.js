9a47f828f75eebabc334fe728d98134f145c5f4b
Krzysztof Bański <krzys0100@wp.pl>
2022-03-18 18:21:36 +0100

    Changed default scene

--- js/scene.js
+++ js/scene.js
@@ -1,8 +1,8 @@
 const aspectRatio = 1280 / 720;
 
 const mainCamera = new Camera(
-    new Vector(50, 4, 0),
-    new Vector(0, 0, 0),
+    new Vector(0, -10, 50),
+    new Vector(0, -10, 0),
     new Vector(0, 1, 0),
     40.0,
     aspectRatio,
@@ -14,10 +14,11 @@ const redLamb = new Lambertian(new Vector(0.9, 0.1, 0.15));
 const blueLamb = new Lambertian(new Vector(0.05, 0.15, 0.85));
 const greenLamb = new Lambertian(new Vector(0.05, 0.85, 0.05));
 
+//flipped vertically :///
 const sceneProps = [
-    new Sphere(new Vector(0, -10, 0), 10, redLamb),
-    new Sphere(new Vector(0, 10, 0), 8, blueLamb),
-    new Sphere(new Vector(0, 0, -16), 8, greenLamb)
+    new Sphere(new Vector(5, -10, -10), 10, redLamb),
+    new Sphere(new Vector(-5, -4, 0), 4, blueLamb),
+    new Sphere(new Vector(0, 1000, 0), 1000, greenLamb)
 ];
 
 const lights = [
